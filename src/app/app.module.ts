fddd3ada2ee12593619dd6c33582af8b09ed1cff
Guilherme Maciel <guilherme.mpereira@ambevtech.com.br>
2022-03-02 13:00:37 -0300

    added product-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { IconCartComponent } from './components/icons/icon-cart/icon-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    ProductPageComponent,
+    IconCartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/icons/icon-cart/icon-cart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-cart',
+  templateUrl: './icon-cart.component.html'
+})
+export class IconCartComponent {
+}
